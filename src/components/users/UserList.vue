cd0401782b794e663c3b277d9ddab078a789c61c
Annak506 <annakleinloog@hotmail.com>
2024-12-03 09:18:27 +0300

    migrated the userlist component

--- src/components/users/UserList.vue
+++ src/components/users/UserList.vue
@@ -20,67 +20,81 @@
 
 <script>
 import UserItem from './UserItem.vue';
+import { ref, computed, watch } from 'vue';
 
 export default {
   components: {
     UserItem,
   },
   props: ['users'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-      sorting: null,
-    };
-  },
-  computed: {
-    availableUsers() {
-      let users = [];
-      if (this.activeSearchTerm) {
-        users = this.users.filter((usr) =>
-          usr.fullName.includes(this.activeSearchTerm)
+  emits: ['list-projects'],
+  setup(props) {
+    // Search logic
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+
+    // Watcher to debounce search term updates
+    watch(enteredSearchTerm, function (newValue) {
+      setTimeout(() => {
+        if (newValue === enteredSearchTerm.value) {
+          activeSearchTerm.value = newValue;
+        }
+      }, 300);
+    });
+
+    // Computed property for filtered users
+    const availableUsers = computed(() => {
+      if (activeSearchTerm.value) {
+        return props.users.filter((usr) =>
+          usr.fullName.toLowerCase().includes(activeSearchTerm.value.toLowerCase())
         );
-      } else if (this.users) {
-        users = this.users;
       }
-      return users;
-    },
-    displayedUsers() {
-      if (!this.sorting) {
-        return this.availableUsers;
+      return props.users || [];
+    });
+
+    // Sorting logic
+    const sorting = ref(null);
+
+    // Computed property for displayed (filtered + sorted) users
+    const displayedUsers = computed(() => {
+      if (!sorting.value) {
+        return availableUsers.value;
       }
-      return this.availableUsers.slice().sort((u1, u2) => {
-        if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
+      return availableUsers.value.slice().sort((u1, u2) => {
+        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
           return 1;
-        } else if (this.sorting === 'asc') {
+        } else if (sorting.value === 'asc') {
           return -1;
-        } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
+        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
           return -1;
         } else {
           return 1;
         }
       });
-    },
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-    sort(mode) {
-      this.sorting = mode;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
-      setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
-        }
-      }, 300);
+    });
+
+    // Sort function
+    function sort(mode) {
+      sorting.value = mode;
     }
+
+    // Function to update the search term
+    function updateSearch(val) {
+      enteredSearchTerm.value = val;
+    }
+
+    // Return variables and functions for template usage
+    return {
+      enteredSearchTerm,
+      updateSearch,
+      displayedUsers,
+      sorting,
+      sort,
+    };
   },
 };
 </script>
+
 
 <style scoped>
 ul {
